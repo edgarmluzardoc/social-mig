2f5396b6a96c9c0d1befad3c97e9aee989a9824e
Miguel <miguel@Edgars-MacBook-Pro.local>
2018-03-05 00:11:04 +1000

    Changing designs 1

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Social Sports</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href='http://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="{{ URL::to('css/styles.css') }}">
+</head>
+<body>
+    <div class="container">
+
+        <!-- Static navbar -->
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">Home</a>
+              <a class="navbar-brand" href="#">Account</a>
+              <a class="navbar-brand" href="#">Friends</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Sign in</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+  
+        <!-- Main body -->
+        @yield('content')
+
+    </div>
+</body>
+</html>
