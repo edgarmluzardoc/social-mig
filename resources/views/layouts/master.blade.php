1e6ae46f10f3789b596988b719983da21fcff7e3
Miguel <miguel@Edgars-MacBook-Pro.local>
2018-03-09 11:59:06 +1000

    Adding reset password feature

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,8 +10,6 @@
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  {{--  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css"
-    integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">  --}}
   <link rel="stylesheet" href="{{ URL::to('css/styles.css') }}">
 
   {{--  Google Fonts  --}}
