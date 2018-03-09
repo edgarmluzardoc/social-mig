20105c76196dd4b6d559d2c14b4b37a5294069fd
Miguel <miguel@Edgars-MacBook-Pro.local>
2018-03-09 15:45:33 +1000

    Adding confirmation message before deleting and friends designs

--- resources/views/friends/index.blade.php
+++ resources/views/friends/index.blade.php
@@ -13,30 +13,58 @@
         </div>
         <div class="col-md-3">
             <form action="{{ route('friend.remove') }}" method="post">
-                <label for="title">Friends</label>
-                @foreach($friends as $friend)
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="friends[]" value="{{ $friend->id }}">{{ $friend->name }}
-                        </label>
-                    </div>
-                @endforeach
+                <table class="table no-border">
+                    <tr>
+                        <th class="center">Friends</th>
+                    </tr>
+                    <tr>
+                        <td class="friends-row">
+                            @if(count($friends) > 0)
+                                @foreach($friends as $friend)
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="friends[]" value="{{ $friend->id }}">{{ $friend->name }}
+                                    </label>
+                                </div>
+                                @endforeach
+                            @else
+                                No friends found.
+                            @endif
+                        </td>
+                    </tr>
+                    <tr class="center">
+                        <td><button type="submit" class="btn btn-primary">Remove</button></td>
+                    </tr>
+                </table>
                 {{ csrf_field() }}
-                <button type="submit" class="btn btn-primary">Remove</button>
             </form>
         </div>
         <div class="col-md-3">
             <form action="{{ route('friend.add') }}" method="post">
-                <label for="title">Users</label>
-                @foreach($users as $user)
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="users[]" value="{{ $user->id }}">{{ $user->name }}
-                        </label>
-                    </div>
-                @endforeach
+                <table class="table no-border">
+                    <tr>
+                        <th class="center">Users</th>
+                    </tr>                    
+                    <tr>
+                        <td class="friends-row">
+                            @if(count($users) > 0)
+                                @foreach($users as $user)
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="users[]" value="{{ $user->id }}">{{ $user->name }}
+                                    </label>
+                                </div>
+                                @endforeach
+                            @else
+                                No users found.
+                            @endif
+                        </td>
+                    </tr>
+                    <tr class="center">
+                        <td><button type="submit" class="btn btn-primary">Add</button></td>
+                    </tr>
+                </table>
                 {{ csrf_field() }}
-                <button type="submit" class="btn btn-primary">Add</button>
             </form>
         </div>
         <div class="col-md-3">
